196fab38884bf97fb7b1385dcc5d7f0da1285b5d
TKOFFICE\huxw <huxw@mail.jj.cn>
2021-08-02 10:46:52 +0800

    支持下载文章

--- download.py
+++ download.py
@@ -17,6 +17,7 @@ headers = {
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8'
 }
 
+
 def getColumn(cid):
     res = requests.get(URL.get("column") + "?column_id=" +
                        cid, headers=headers)
@@ -27,7 +28,7 @@ def getArticleList(cid):
     data = {
         "column_id": cid,
         "cursor": "0",
-        "limit": 1000,
+        "limit": 20,
         "sort": 0
     }
     res = requests.post(URL.get("articles"),
@@ -43,12 +44,17 @@ def getArticleContent(aid):
     return res.json()
 
 
+def ensureDir(dir):
+    os.makedirs(dir, mode=0o777, exist_ok=True)
+
+
 def saveColumn(cid, content):
     folder = "./datas/%s" % cid
     file = "%s/%s.json" % (folder, "column")
-    os.makedirs(folder, mode=0o777, exist_ok=True)
+    ensureDir(folder);
     with open(file, "w", encoding="utf-8") as f:
         f.write(content)
+
 
 def saveArticleList(cid, content):
     folder = "./datas/%s" % cid
@@ -64,22 +70,21 @@ def saveArticleContent(cid, aid, content):
         f.write(content)
 
 
-
 def downloadColumn(cid):
 
-    column = getColumn(cid);
-    print("获取专栏信息成功，专栏名称：", column["data"]["column_version"]["title"]);
+    column = getColumn(cid)
+    print("获取专栏信息成功，专栏名称：", column["data"]["column_version"]["title"])
     saveColumn(cid, json.dumps(column["data"]))
 
     articleList = getArticleList(cid)
+    print("articleList", articleList)
     saveArticleList(cid, json.dumps(articleList["data"]))
 
-    articleIds = list(map(lambda item: item["article_id"], articleList["data"]))
+    articleIds = list(
+        map(lambda item: item["article_id"], articleList["data"]))
     print("获取专栏文章列表成功：", articleIds)
 
     for artId in articleIds:
         artContent = getArticleContent(artId)
         saveArticleContent(cid, artId, json.dumps(artContent["data"]))
         print("文章ID为 %s 的文章下载完毕" % artId)
-
-
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import download
 import mdToHtml
 import subprocess
+import json
+import sys
 
 
 def genPdf(title):
@@ -12,10 +14,6 @@ def genPdf(title):
         exePath, sourcePath, targetPath)
     print(cmd)
     subprocess.call(cmd)
-
-
-# C_ID = '6979380367216082957'
-C_ID = '6977915766267969549'
 
 
 def downloadAndGenPdf(cid):
@@ -30,4 +28,46 @@ def downloadAndGenPdf(cid):
     print("%s.pdf生成完毕" % title)
 
 
-downloadAndGenPdf(C_ID)
+def downloadArticleAndGenPdf(aid):
+    resArt = download.getArticleContent(aid)
+    artContent = resArt["data"]
+    folder = "./datas/%s" % aid
+    download.ensureDir(folder)
+
+    download.saveArticleContent(aid, aid,  json.dumps(artContent))
+    download.saveArticleList(aid,  json.dumps([{
+        "article_id": artContent["article_id"]
+    }]))
+    title = artContent["article_info"]["title"]
+    print("开始生成html文件")
+    mdToHtml.mdToHtml(aid, title)
+    print("生成html文件完毕")
+    print("开始生成%s.pdf" % title)
+    genPdf(title)
+    print("%s.pdf生成完毕" % title)
+
+
+C_ID = '6979380367216082957'
+A_ID = '6991628964573741093'
+
+# downloadAndGenPdf(C_ID)
+# downloadArticleAndGenPdf("6991628964573741093")
+
+
+def run():
+
+    if len(sys.argv) < 2:
+        return print("please input columnId or articleId");
+
+    acid = sys.argv[1]
+    actype = "c" if len(sys.argv) < 3 else sys.argv[2];
+
+    print("acid:%s  acttype:%s" % (acid, actype));
+
+    if actype == "a":
+        downloadArticleAndGenPdf(acid)
+    else:
+        downloadAndGenPdf(acid)
+
+
+run();
